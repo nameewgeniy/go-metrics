73ce486b3c642ebf0c7e788b30ef411c60dc49d9
Nakapyuk Evgeniy <nameewgeniy@ya.ru>
2023-09-13 01:53:08 +0300

    add env

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
 )
 
 type flags struct {
 	pushAddress       string
 	pollIntervalSec   int
 	reportIntervalSec int
+}
+
+func (f *flags) validate() error {
+	if _, _, err := net.SplitHostPort(f.pushAddress); err != nil {
+		return fmt.Errorf("address is not valid: %s", err)
+	}
+
+	if f.pollIntervalSec < 1 {
+		return fmt.Errorf("poll interval must be greater than 1, current: %d", f.pollIntervalSec)
+	}
+
+	if f.reportIntervalSec < 1 {
+		return fmt.Errorf("report interval must be greater than 1, current: %d", f.reportIntervalSec)
+	}
+
+	return nil
 }
 
 func parseFlags() (*flags, error) {
@@ -18,6 +38,33 @@ func parseFlags() (*flags, error) {
 	flag.IntVar(&f.reportIntervalSec, "r", 10, "report interval in seconds")
 
 	flag.Parse()
+
+	pAddr := os.Getenv("ADDRESS")
+	if pAddr != "" {
+		f.pushAddress = pAddr
+	}
+
+	pi := os.Getenv("POLL_INTERVAL")
+	if pi != "" {
+		v, err := strconv.Atoi(pi)
+		if err != nil {
+			return nil, fmt.Errorf("POLL_INTERVAL is not valid: %s", err)
+		}
+		f.pollIntervalSec = v
+	}
+
+	ri := os.Getenv("REPORT_INTERVAL")
+	if ri != "" {
+		v, err := strconv.Atoi(ri)
+		if err != nil {
+			return nil, fmt.Errorf("REPORT_INTERVAL is not valid: %s", err)
+		}
+		f.reportIntervalSec = v
+	}
+
+	if err := f.validate(); err != nil {
+		return nil, err
+	}
 
 	return &f, nil
 }
