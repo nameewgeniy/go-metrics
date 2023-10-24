3e7e0ec198332b3d94357b901d3c06655199194e
Nakapyuk Evgeniy <nameewgeniy@ya.ru>
2023-10-25 00:12:53 +0300

    change timeout

--- internal/server/storage/pg/ping.go
+++ internal/server/storage/pg/ping.go
@@ -7,7 +7,7 @@ import (
 
 func (p Pg) Ping() error {
 
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	return p.c.DB().PingContext(ctx)
